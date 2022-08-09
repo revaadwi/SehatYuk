1d92fa5d4402c497a5f3f4c2b463823305419ed8
revaadwi <xrevadf@gmail.com>
2022-08-09 11:22:19 +0700

    change camera orientation

--- app/src/main/java/com/app/sehatyuk/WebViewActivity.kt
+++ app/src/main/java/com/app/sehatyuk/WebViewActivity.kt
@@ -3,7 +3,10 @@ package com.app.sehatyuk
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.webkit.WebViewClient
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_web_view.*
 
 class WebViewActivity : AppCompatActivity() {
@@ -11,6 +14,11 @@ class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+
+        supportActionBar?.hide()
+        val window: Window = this@WebViewActivity.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this@WebViewActivity, R.color.blue)
 
         wv_result.webViewClient = WebViewClient()
         wv_result.loadUrl(intent.getStringExtra("url")?:"")
